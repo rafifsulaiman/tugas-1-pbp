4ee3c2a957bea1c66fbd3e9fbea71dbfd95cd743
rafifsulaiman <rafifsulaimann@gmail.com>
2024-09-11 11:57:35 +0700

    push again

--- main/models.py
+++ main/models.py
@@ -7,3 +7,6 @@ class Product(models.Model):
     price = models.IntegerField()
     description = models.TextField(max_length=300)
     image = models.ImageField() #upload image
+    person_name = models.CharField(max_length=300)
+    person_class = models.TextField()
+    person_npm = models.IntegerField()
--- main/views.py
+++ main/views.py
@@ -6,7 +6,10 @@ def show_products(request):
         'name' : 'Ultra Milk 1 L Full Cream',
         'price' : 20000,
         'description' : 'Ultra milk 1L full cream with plain flavor',
-        'image' : 'https://i.imgur.com/Xe74ORV.jpeg'
+        'image' : 'https://i.imgur.com/Xe74ORV.jpeg',
+        'person_name' : 'Rafif Sulaiman Dirvesa',
+        'person_class' : 'PBP C',
+        'person_npm' : '2306222771'
     }
 
     return render(request, 'main.html', content)
